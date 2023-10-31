b95b9bd32a01627161bbf0b6b601e710f7f8eeb6
turecross321 <51852312+turecross321@users.noreply.github.com>
2023-10-31 17:54:21 +0100

    Implement changing email

---
+++ src/app/pages/change-email-page/change-email-page.component.ts
@@ -0,0 +1,54 @@
+import {Component} from '@angular/core';
+import {IconDefinition} from "@fortawesome/fontawesome-svg-core";
+import {FormBuilder} from "@angular/forms";
+import {ApiClientService} from "../../api/api-client.service";
+import {FormValidity} from "../../types/form-validity";
+import {validEmail, validEmailCode} from "../../regex";
+import {ElementStyle} from 'src/app/types/element-style';
+import {faEnvelope, faHashtag, faPen} from '@fortawesome/free-solid-svg-icons';
+import {Router} from "@angular/router";
+
+@Component({
+    selector: 'app-change-email-page',
+    templateUrl: './change-email-page.component.html',
+    styleUrls: []
+})
+export class ChangeEmailPageComponent {
+    faHashtag: IconDefinition = faHashtag;
+    faEnvelope: IconDefinition = faEnvelope;
+    form = this.formBuilder.group({emailCode: '', newEmail: ''});
+    changingEmail: boolean = false;
+    faPen: IconDefinition = faPen;
+    protected readonly ElementStyle = ElementStyle;
+
+    constructor(private formBuilder: FormBuilder, private apiClient: ApiClientService, private router: Router) {
+    }
+
+    validForm(): FormValidity {
+        const code = this.form.get("emailCode")?.value!;
+        const email = this.form.get("newEmail")?.value!;
+
+        if (!validEmailCode(code))
+            return {valid: false, message: "Invalid email code."};
+
+        if (!validEmail(email))
+            return {valid: false, message: "Invalid e-mail address."}
+
+        return {valid: true, message: ""};
+    }
+
+    async change() {
+        const code = this.form.get("emailCode")?.value!;
+        const email = this.form.get("newEmail")?.value!;
+        this.changingEmail = true;
+
+        try {
+            await this.apiClient.setEmail(code, email);
+            await this.router.navigateByUrl("/");
+        } catch (e) {
+
+        }
+
+        this.changingEmail = false;
+    }
+}
