8ee8aaf7315dfb6f14e7bf3befda3aaf22c066e1
turecross321 <51852312+turecross321@users.noreply.github.com>
2024-09-18 22:26:23 +0200

    Sticky navbar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {HeaderMeComponent} from "../header-me/header-me.component";
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
-import {faHouse, faMusic} from "@fortawesome/free-solid-svg-icons";
+import {faCompactDisc, faHouse, faMusic, faUsers} from "@fortawesome/free-solid-svg-icons";
 import {NgForOf} from "@angular/common";
 import {NavLinkComponent} from "../nav-link/nav-link.component";
 import {VerticalDividerComponent} from "../vertical-divider/vertical-divider.component";
@@ -23,4 +23,6 @@ export class HeaderComponent {
   protected readonly faHouse = faHouse;
   protected readonly faMusic = faMusic;
   protected readonly ColorType = ColorType;
+  protected readonly faUsers = faUsers;
+  protected readonly faCompactDisc = faCompactDisc;
 }
